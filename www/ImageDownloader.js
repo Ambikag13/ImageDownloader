7708db50e6070977f31b674bb79cd2128db30f5d
unknown <ambikag@ambikag.telxsi.com>
2015-02-06 17:51:57 +0530

    Added options for plugin

--- www/ImageDownloader.js
+++ www/ImageDownloader.js
@@ -1,16 +1,11 @@
 var ImageDownloader = {
     downloadImage: function(successCallback, errorCallback, options) {
-    	//var path = null;
+
         //var assets = null;
-        //var quote = null;
-        //var cutin = null
         
-        /*if (options) {
-        	path = options.path;
+        if (options) {
         	assets = options.assets;
-        	quote = options.quote;
-        	cutin = options.cutin;
-        }*/
+        }
         
         cordova.exec(
             successCallback, // success callback function
